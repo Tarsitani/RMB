9da4b9a4eebce457345309ef396b6c0521bac023
Lucas Tarsitani <lucastarsitani@Spiders-PC.local>
2017-03-31 15:38:18 -0300

    teste

---
+++ remember-project/ButtonScreenCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonScreenCollectionViewCell.swift
+//  remember-project
+//
+//  Created by Lucas Tarsitani on 31/03/17.
+//  Copyright © 2017 Lucas Tarsitani. All rights reserved.
+//
+
+import UIKit
+
+class ButtonScreenCollectionViewCell: UICollectionViewCell {
+    
+}
