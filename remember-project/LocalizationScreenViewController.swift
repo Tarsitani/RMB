bc64018f5a03ba4d345b094c645a5424960e1330
Matheus Garcia <Brother@Matheuss-MacBook-Pro.local>
2017-04-05 10:17:33 -0300

    quem sabe agora

--- remember-project/LocalizationScreenViewController.swift
+++ remember-project/LocalizationScreenViewController.swift
@@ -20,16 +20,6 @@ class LocalizationScreenViewController: UIViewController, UICollectionViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        //Started to set the navigation bar settings
-        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white] //Turn the title white
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default) //Make the background cover the bar
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = true //Turn translucide the bar
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil) //Hide the text in the back button
-        self.navigationController?.navigationBar.tintColor = UIColor.white; //Turn all the buttons white
-        //Finish to set the navigation bar settings
-        
         self.LocalizationScreenCollectionView.delegate = self
         self.LocalizationScreenCollectionView.dataSource = self
         
